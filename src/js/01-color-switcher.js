b0878e00781cfee7aacbfc4c4d92e04d79f56358
Marina Hrytsai <polanic@ukr.net>
2022-01-15 14:44:50 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const DELAY = 1000;
+let timerId = null;
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  body.style.backgroundColor = getRandomHexColor;
+}
+
+startBtn.addEventListener('click', startBtnClick);
+function startBtnClick() {
+  console.log(`Call startBtnClick every ${DELAY} ms`);
+  timerId = setInterval(changeBgColor, DELAY);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+stoptBtn.addEventListener('click', stopBtnClick);
+function stopBtnClick() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  console.log('Stopped');
+}
 
