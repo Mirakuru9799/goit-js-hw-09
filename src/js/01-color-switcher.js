bfbf9d4b690c004f4017d8917f01ad7f72e5c213
Marina Hrytsai <polanic@ukr.net>
2022-01-15 15:24:31 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,11 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
 const DELAY = 1000;
 let timerId = null;
 startBtn.disabled = false;
 stopBtn.disabled = true;
-
-
-const body = document.querySelector('body');
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -18,19 +16,21 @@ function changeBgColor() {
 }
 
 startBtn.addEventListener('click', onStartBtnClick);
+
 function onStartBtnClick() {
   console.log(`Call onStartBtnClick every ${DELAY} ms`);
   timerId = setInterval(changeBgColor, DELAY);
   startBtn.disabled = true;
   stopBtn.disabled = false;
-}
+};
 
 stoptBtn.addEventListener('click', onStopBtnClick);
-function onStopBtnClick() {
-  clearInterval(timerId);
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
 
+function onStopBtnClick() {
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  clearInterval(timerId);
   console.log('Stopped');
 }
 
